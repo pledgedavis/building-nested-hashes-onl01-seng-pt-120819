ab2e87720eb57291fd4f278f289d558669167c86
pledgedavis <pledgedavis14@gmail.com>
2019-12-21 15:12:53 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,10 @@ def bonus
    }
   }
 
-  #code your solution here:
+  #code your solution here: 
+  # epic_tragedy["montague"][:hero][:name] << "dead"
 
-  
+  # contacts["Jon Snow"][:favorite_ice_cream_flavors]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
